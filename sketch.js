130c81c6ab7c4c46f388348db364a2f7578567f8
MrO <posudar@gmail.com>
2018-11-12 18:07:19 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function setup() {
 
 function draw() {
   // Displays the image at its actual size at point (0,0)
-  image(img, 0, 0);
+  image(img, mouseX, mouseY);
   // Displays the image at point (0, height/2) at half size
   image(img, 0, height/2, img.width/2, img.height/2);
 	
