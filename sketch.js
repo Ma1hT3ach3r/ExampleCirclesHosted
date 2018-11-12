783b8562b3e0eb0b74fda3572619363dcdfc5400
MrO <posudar@gmail.com>
2018-11-12 17:52:11 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,16 @@
+var img;  // Declare variable 'img'.
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(720, 400);
+  img = loadImage("ExampleCirclesHosted/Felis_silvestris_silvestris_small_gradual_decrease_of_quality.png");  // Load the image
 }
 
 function draw() {
-  fill(mouseX,mouseY,100);
+  // Displays the image at its actual size at point (0,0)
+  image(img, 0, 0);
+  // Displays the image at point (0, height/2) at half size
+  image(img, 0, height/2, img.width/2, img.height/2);
+	
+	 fill(mouseX,mouseY,100);
 	ellipse(mouseX,mouseY, 100,100);
 }
